0027ace3797efcbb7877c93c430ac71efcfd87be
lucky096 <lucky091296@gmail.com>
2025-01-03 16:23:16 +0530

    add llm model, embeddings model, reranker model

---
+++ model.py
@@ -0,0 +1,28 @@
+from langchain_community.chat_models import ChatOllama
+from langchain_community.document_compressors.flashrank_rerank import FlashrankRerank
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from langchain_core.language_models import BaseLanguageModel
+from langchain_groq import ChatGroq
+
+from config import Config
+
+def create_llm() -> BaseLanguageModel:
+    if Config.Model.USE_LOCAL:
+        return ChatOllama(
+            model=Config.Model.LOCAL_LLM,
+            temperature=Config.Model.TEMPERATURE,
+            keep_alive="1h",
+            max_tokens=Config.Model.MAX_TOKENS,
+        )
+    else:
+        return ChatGroq(
+            model_name=Config.Model.REMOTE_LLM,
+            temperature=Config.Model.TEMPERATURE,
+            max_tokens=Config.Model.MAX_TOKENS,
+        )
+
+def create_embeddings() -> FastEmbedEmbeddings:
+    return FastEmbedEmbeddings(model_name=Config.Model.EMBEDDINGS)
+
+def create_reranker() -> FlashrankRerank:
+    return FlashrankRerank(model=Config.Model.RERANKER)
